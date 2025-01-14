3e4a6f4f6e186c8589d64ac1a1bb1232b4c930d5
Oguz Yetkin <oyetkin@gmail.com>
2025-01-14 23:55:45 +0300

    fixed typo

--- otp_decode.sh
+++ otp_decode.sh
@@ -1,4 +1,4 @@
 python3 recordQT.py
 ./decode.sh dtmf.wav >otp_encrypted_output.txt
-python3 python otp_crypto_audio.py -d "$(cat otp_encrypted_output.txt)" >otp_output.txt
+python3 otp_crypto_audio.py -d "$(cat otp_encrypted_output.txt)" >otp_output.txt
 python3 messagebox1.py "$(cat otp_output.txt)"
