6fc2fc3d4295e94630761187428c87411ffd15e2
cbalik <caglarbalik1@gmail.com>
2025-01-14 18:23:14 +0300

    modified for audio

--- get_input.py
+++ get_input.py
@@ -1,52 +1,58 @@
 #!/usr/bin/python
 
-#kaynak: https://stackoverflow.com/questions/38725442/how-to-use-tkinter-entry-like-raw-input
 import tkinter as tk
-w = 800 # width for the Tk root
-h = 650 # height for the Tk root
+import os
 
-#https://stackoverflow.com/questions/35817/how-to-escape-os-system-calls
+w = 800  # width for the Tk root
+h = 100  # height for the Tk root
+
+def play():
+    os.popen('/usr/bin/bash playsound.sh')
+
 def sh_escape(s):
-   return s.replace("(","\\(").replace(")","\\)").replace(" ","\\ ")
+    return s.replace("(", "\\(").replace(")", "\\)").replace(" ", "\\ ")
 
 def gui_input(prompt):
-
     root = tk.Tk()
-    w = 600 # width for the Tk root
-    h = 100 # height for the Tk root
+    w = 600  # width for the Tk root
+    h = 150  # keep the original height
 
-    # get screen width and height
-    ws = root.winfo_screenwidth() # width of the screen
-    hs = root.winfo_screenheight() # height of the screen
-    x=20
-    y=20
+    # Get screen width and height
+    ws = root.winfo_screenwidth()  # width of the screen
+    hs = root.winfo_screenheight()  # height of the screen
+    x = 20
+    y = 20
 
-# set the dimensions of the screen 
-# and where it is placed
-    #print('%dx%d+%d+%d' % (w, h, x,y))
+    # Set the dimensions of the screen and where it is placed
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    # this will contain the entered string, and will
-    # still exist after the window is destroyed
+
+    # This will contain the entered string, and will still exist after the window is destroyed
     var = tk.StringVar()
 
-    # create the GUI
+    # Create the GUI
     label = tk.Label(root, text=prompt)
     entry = tk.Entry(root, textvariable=var)
+
+    # Create a frame to hold the widgets
+    frame = tk.Frame(root)
+    frame.pack(fill="both", expand=True)
+
+    # Place the label and entry within the frame
     label.pack(side="left", padx=(20, 0), pady=20)
     entry.pack(side="right", fill="x", padx=(0, 20), pady=20, expand=True)
 
-    # Let the user press the return key to destroy the gui 
-    entry.bind("<Return>", lambda event: root.destroy())
+    # Create and place the play button at the bottom
+    play_button = tk.Button(frame, text="Şifrelenmiş Sesi Çal", font=("Helvetica", 16), command=play)
+    play_button.pack(side="bottom", pady=10)
 
-    # this will block until the window is destroyed
+
+    # This will block until the window is destroyed
     root.mainloop()
 
-    # after the window has been destroyed, we can't access
-    # the entry widget, but we _can_ access the associated
-    # variable
+    # After the window has been destroyed, we can't access the entry widget,
+    # but we _can_ access the associated variable
     value = var.get()
     return value
 
-mesaj = gui_input("Sifrelenecek Mesaj").encode('utf-8')
-
+mesaj = gui_input("Sifrelenecek Mesaj")
 print(mesaj)
